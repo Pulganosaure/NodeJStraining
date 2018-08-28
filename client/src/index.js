7591ce00aa7f3968592fc1ac10a4804120abecfd
sc-pulgan <38398757+sc-pulgan@users.noreply.github.com>
2018-08-28 20:41:38 +0200

    adding redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import rooReducer from './reducers'
+import middleware from './middleware'
+
+
+const store = createStore(rooReducer, composeWithDevTools(middleware))
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
