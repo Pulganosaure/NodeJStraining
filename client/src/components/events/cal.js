fdae1550512ae65cb5a558d72bd90335bee0f4e1
Erwan <rr.1.riou@gmail.com>
2018-10-08 02:36:32 +0200

    Clean de la page

--- client/src/components/events/cal.js
+++ client/src/components/events/cal.js
@@ -1,16 +1,14 @@
 import React, {Fragment} from 'react'
 import EventMenuDisplayer from './eventMenuDisplayer.js'
+import moment from 'moment'
 import '../../style/calendar.css'
-
-var moment = require('moment')
-
 
 class Cal extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-
+      year: new Date().getYear(),
       month: new Date().getMonth(),
       calendar: [],
       events: [1, 2, 5]
@@ -21,106 +19,90 @@ class Cal extends React.Component {
   }
 
 
-  renderHeader()
-  {
+  renderHeader() {
     let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
     return (
       <tr>
-        { days.map( (day, key) =>
+        {days.map((day, key) =>
           <td key={key} className="CalendarCell">
-
             <span>{day}</span>
-
-          </td>)
-        }
+          </td>)}
       </tr>
     )
   }
-  renderMonth()
-  {
+  renderMonth() {
     return (
       <div className="CalendarMonthHeader">
         <button className="brah"
           //onClick={() => this.state.month === 0 ?this.setState({month: 12}) : this.setState({month: this.state.month-1})}
-          onClick={() => this.setState({month: this.state.month-1})}
-          >
-            previous
-          </button>
-          <h1 className="brah">{moment().month(this.state.month).format('MMMM YYYY').toString()}</h1>
-          <button className="brah"
-            onClick={() =>  this.setState({month: this.state.month+1})}
-            //onClick={() => this.state.month === 12 ?this.setState({month: 0}) : this.setState({month: this.state.month+1})}
-            >
-              next
-            </button>
-          </div>
-        )
-      }
+          onClick={() => this.setState({month: this.state.month-1})}>
+          previous
+        </button>
+        <h1 className="brah">{moment().month(this.state.month).format('MMMM YYYY').toString()}</h1>
+        <button className="brah"
+          //onClick={() => this.state.month === 12 ?this.setState({month: 0}) : this.setState({month: this.state.month+1})}
+          onClick={() =>  this.setState({month: this.state.month+1})}>
+          next
+        </button>
+      </div>
+    )
+  }
 
-      renderCell()
-      {
-        let startWeek = moment().startOf('month').year(2019).month(this.state.month).isoWeek();
-        let endWeek = moment().endOf('month').year(2019).month(this.state.month).isoWeek();
-        //in case if the december have 1 in endWeek
-        if(endWeek < startWeek)
-          endWeek = startWeek+5
+  renderCell() {
+    let startWeek = moment().startOf('month').year('year').month(this.state.month).isoWeek();
+    let endWeek = moment().endOf('month').year('year').month(this.state.month).isoWeek();
+    //in case if the december have 1 in endWeek
+    if(endWeek < startWeek) endWeek = startWeek+5
+    let calendar = []
+    for(var week = startWeek; week <= endWeek;week++) {
+      calendar.push({
+        week: week,
+        days: Array(7).fill(0).map((n, i) => moment().isoWeek(week).startOf('isoweek').add(n + i , 'day'))
+      })
 
-        let calendar = []
-        for(var week = startWeek; week <= endWeek;week++){
-          calendar.push({
-            week:week,
-            days: Array(7).fill(0).map((n, i) => moment().isoWeek(week).startOf('isoweek').add(n + i , 'day'))
-          })
-
-
-        }
+    }
+          console.log(calendar);
+    return (
+      calendar.map((week, key) => {
         return (
-          calendar.map((week, key) => {
-            return (
-              <tr key={key} className="CalendarRow">
-                {
-                  week.days.map((day, key) => {
-                    console.log(this.state.month + ' | ' + day.month())
-                    console.log(day.year())
-                    return (
-                      day.month() === (this.state.month % 12)
-
-                      ? <td key={key} className="CalendarCell">
-                          <span>{day.date()}</span>
-                          {
-                            (this.state.events.includes(day.date()))
-                          ?
-                            <div className="event">
-                              Event prévu
-                            </div>
-                          : null
-                          }
-                        </td>
-                      : <td key={key} className="CalendarCell celldisabled">
-                          <span>{day.date()}</span>
-                        </td>
-                    )})
-                  }
-                </tr>
-              )}))
-            }
-
-            render()
-            {
-
+          <tr key={key} className="CalendarRow"> {
+            week.days.map((day, key) => {
+              console.log(this.state.month + ' | ' + day.month())
+              console.log(day.year())
               return (
-                <Fragment>
-                  {this.renderMonth()}
-                  <table>
-                    <thead>
-                      {this.renderHeader()}
-                    </thead>
-                    <tbody>
-                      {this.renderCell()}
-                    </tbody>
-                  </table>
-                </Fragment>)
-              }
+                day.month() === (this.state.month % 12)
+                ? <td key={key} className="CalendarCell">
+                    <span>{day.date()}</span>
+                    {(this.state.events.includes(day.date()))
+                      ? <div className="event">
+                          Event prévu
+                        </div>
+                      : null }
+                  </td>
+                : <td key={key} className="CalendarCell celldisabled">
+                    <span>{day.date()}</span>
+                  </td>
+              )})
             }
+        </tr>
+    )}))
+  }
 
-            export default Cal
+  render() {
+    return (
+      <Fragment>
+        {this.renderMonth()}
+        <table>
+          <thead>
+            {this.renderHeader()}
+          </thead>
+          <tbody>
+            {this.renderCell()}
+          </tbody>
+        </table>
+      </Fragment>
+    )
+  }
+}
+
+export default Cal
